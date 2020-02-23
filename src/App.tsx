8247ad0302a972711c064f32ff8e65f4290cdd61
mateja <mateja176@gmail.com>
2020-02-23 13:34:07 +0100

    feat: adds push.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
+// @ts-ignore
+import Push from 'push.js';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const push = () => {
+    Push.create('Hello world!', {
+      body: "How's it going?",
+      icon: '/logo192.png',
+      timeout: 4000,
+      onClick: ({ target }: Event) => {
+        if (target) {
+          const notification = target as Notification;
+          window.focus();
+          notification.close();
+        }
+      },
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={push}>push</button>
     </div>
   );
 }
